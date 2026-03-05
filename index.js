226a1e3c9a71fb809b3bb608fcf46ea9ddf6cdd8
Frnc36 <magoriferenc19@gmail.com>
2026-03-05 13:29:55 +0100

    tovabb

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
+let kElemek = document.querySelectorAll("img");
+
 let kNev = document.getElementById("nev");
 kNev.innerHTML = "Mágori Ferenc Ferdinánd";
 
 let tNev = document.querySelector("title");
 tNev.innerHTML = "Mágori Ferenc Ferdinánd";
 
+let jobbElem = document.getElementById("jobb");
+let csonakElem = document.getElementById("csonak");
+const csonakLista = [];
 
-let kElemek = document.querySelectorAll("img")
+/* függvények */
+kiemeltKeret(kElemek);
+listaba(kElemek);
 
-for (let index = 0; index < kElemek.length; index++) {
+function kiemeltKeret() {
+  for (let index = 0; index < kElemek.length; index++) {
+    kElemek[index].addEventListener("mouseover", function () {
+      kElemek[index].classList.add("kiemelt");
+    });
 
+    kElemek[index].addEventListener("mouseout", function () {
+      kElemek[index].classList.remove("kiemelt");
+    });
+  }
 }
-kElemek[0].addEventListener("mouseover", function () {
-    kElemek[0].classList.add("kiemelt")
-})
 
-kElemek[0].addEventListener("mouseout", function () {
-    kElemek[0].classList.remove("kiemelt")
-})
+function listaba(kElemek) {
+  for (let index = 0; index < kElemek.length; index++) {
+    kElemek[index].addEventListener("click", function () {
+      kElemek[index].style.display = "none";
+      //event.target.style.display = "none";
+      console.log(kElemek[index]);
+      csonakLista.push(kElemek[index].src);
+      //csonakLista.push(event.target.src);
+      //let kep = kElemek[index].src;
+      //csonakLista.push(kep);
+      csonakba();
+    });
+  }
+}
+
+function csonakba() {
+  let szoveg = "";
+  for (let index = 0; index < csonakLista.length; index++) {
+    szoveg += `<img src="${csonakLista[index]}" class="meret">`;
+    //console.log(csonakLista[index]);
+    csonakElem.innerHTML = szoveg;
+  }
+}
+
+/* (tomb.push(event.target.src)) 
+szovegElem[1].classList.add("valami");*/
